22a625a74f3214f207fc705fca9283cebfb70d36
TheTimeLord <brianandrus2@gmail.com>
2021-04-27 09:51:45 -0700

    Inital Commit

---
+++ src/test/java/com/techbee/springBootUserProfile/SpringBootUserProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techbee.springBootUserProfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
